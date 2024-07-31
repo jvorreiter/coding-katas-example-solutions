57ecb8acf1ad8f796f1d24418fab63da48c517d5
jvorreiter <jvorreiter@inmediasp.de>
2024-07-31 12:02:16 +0200

    rename maxSpeed to maxSpeedKmh

--- src/algodat/v_graph_pathfinding/_core/DirectedConnection.java
+++ src/algodat/v_graph_pathfinding/_core/DirectedConnection.java
@@ -1,4 +1,4 @@
 package algodat.v_graph_pathfinding._core;
 
-public record DirectedConnection(int fromNodeId, int toNodeId, int maxSpeed) {
+public record DirectedConnection(int fromNodeId, int toNodeId, int maxSpeedKmh) {
 }
